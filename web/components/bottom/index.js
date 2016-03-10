23d6eaf9bf623155740df78a2288bcbf9baeccb0
joahCN <448363903@qq.com>
2016-03-10 22:55:46 +0800

    add bottom panel

---
+++ web/components/bottom/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 2016/3/10.
+ */
+import {React} from "../../elements/core"
+import "./_style.scss"
+
+export default ()=>{
+    return <div className="page-bottom">
+        copyright@Joah.Zhang
+
+    </div>
+}
